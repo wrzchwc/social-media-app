21227a7cb7e51c1f530ff77b383d7c46fe2131b0
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2021-12-29 23:47:21 +0100

    Store reorganization

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import {reducer as formReducer} from "redux-form";
 import registerReducer from "./registerReducer";
 
 export default combineReducers({
-    token: loginReducer,
     form: formReducer,
-    registration: registerReducer
+    registration: registerReducer,
+    token: loginReducer
 })
