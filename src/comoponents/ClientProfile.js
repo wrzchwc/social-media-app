3b2211e32da8d316ea4179a862e6f02ee501062f
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-01-11 15:40:46 +0100

    ClientProfile converted to function component

--- src/comoponents/ClientProfile.js
+++ src/comoponents/ClientProfile.js
@@ -1,5 +1,4 @@
-import React from "react";
-import '../styles/Layout.css';
+import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {fetchClient} from "../actions";
 import {months} from './months';
@@ -9,14 +8,15 @@ import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 import EmailIcon from '@mui/icons-material/Email';
 import CakeIcon from '@mui/icons-material/Cake';
 
-class ClientProfile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.props.fetchClient();
-    }
+const ClientProfile = ({client, fetchClient}) => {
 
-    renderButtons() {
-        let {followers, following} = this.props.client;
+    useEffect(() => {
+        fetchClient()
+    }, [fetchClient]);
+
+
+    const renderButtons = () => {
+        let {followers, following} = client;
         try {
             return (
                 <div className="flex" style={{justifyContent: 'space-around'}}>
@@ -28,85 +28,84 @@ class ClientProfile extends React.Component {
         }
     }
 
-    renderBirthdate() {
-        let {dateOfBirth} = this.props.client;
+
+    const renderBirthdate = () => {
+        let {dateOfBirth} = client;
         try {
             return `${dateOfBirth[2]} ${months(dateOfBirth[1])} ${dateOfBirth[0]}`;
         } catch (e) {
         }
     }
 
-    getInitials(){
-        let {client} = this.props;
+    const getInitials = () => {
+        let {name, surname} = client;
         try {
-            return client.name.substr(0, 1).concat(client.surname.substr(0, 1));
+            return name.substr(0, 1).concat(surname.substr(0, 1));
         } catch (e) {
             return '?'
         }
     }
 
-    render() {
-        let {client} = this.props;
-
-        return (
-            <Grid container style={{minHeight: "89vh"}}>
-                <Grid
-                    item
-                    container
-                    justifyContent={"center"}
-                    alignItems={"flex-start"}
-                >
-                    <Grid item container xs={4} direction={"column"} alignItems={"center"} spacing={2} style={{marginTop: "3em"}}>
+    return (
+        <Grid container style={{minHeight: "89vh"}}>
+            <Grid
+                item
+                container
+                justifyContent={"center"}
+                alignItems={"flex-start"}
+            >
+                <Grid item container xs={4} direction={"column"} alignItems={"center"} spacing={2}
+                      style={{marginTop: "3em"}}>
+                    <Grid item>
+                        <Avatar
+                            sx={{
+                                bgcolor: `${Theme.palette.common.cyan}`,
+                                color: `${Theme.palette.common.violet}`,
+                                fontSize: "4em",
+                                height: "2em",
+                                width: "2em"
+                            }}
+                        >
+                            {getInitials()}
+                        </Avatar>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant={"h3"} style={{fontWeight: 300}}>
+                            {`${client.name} ${client.surname}`}
+                        </Typography>
+                    </Grid>
+                    <Grid item container alignItems={"center"} justifyContent={"center"}>
                         <Grid item>
-                            <Avatar
-                                sx={{
-                                    bgcolor: `${Theme.palette.common.cyan}`,
-                                    color: `${Theme.palette.common.violet}`,
-                                    fontSize: "4em",
-                                    height: "2em",
-                                    width: "2em"
-                                }}
-                            >
-                                {this.getInitials()}
-                            </Avatar>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant={"h3"} style={{fontWeight: 300}}>
-                                {`${client.name} ${client.surname}`}
+                            <Typography variant={"p"} style={{fontWeight: 250}}>
+                                {client.username} <VerifiedUserIcon color={"success"}
+                                                                    style={{verticalAlign: "bottom"}}/>
                             </Typography>
                         </Grid>
-                        <Grid item container alignItems={"center"} justifyContent={"center"}>
-                            <Grid item>
-                                <Typography variant={"p"} style={{fontWeight: 250}} >
-                                    {client.username} <VerifiedUserIcon color={"success"} style={{verticalAlign: "bottom"}}/>
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                {this.renderButtons()}
-                            </Grid>
-                        </Grid>
-                        <Grid item container direction={"column"} justifyContent={"center"} alignItems={"flex-start"} alignContent={"center"} spacing={2}>
-                            <Grid item>
-                                <Typography variant={"p"} style={{fontWeight: 250}}>
-                                    <EmailIcon color={"primary"} style={{verticalAlign: "bottom"}}/> {client.email}
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant={"p"} style={{fontWeight: 250}}>
-                                    <CakeIcon color={"primary"} style={{verticalAlign: "bottom"}}/> {this.renderBirthdate()}
-                                </Typography>
-                            </Grid>
+                        <Grid item>
+                            {renderButtons()}
                         </Grid>
                     </Grid>
-                    <Grid item xs={8}>
-
+                    <Grid item container direction={"column"} justifyContent={"center"} alignItems={"flex-start"}
+                          alignContent={"center"} spacing={2}>
+                        <Grid item>
+                            <Typography variant={"p"} style={{fontWeight: 250}}>
+                                <EmailIcon color={"primary"} style={{verticalAlign: "bottom"}}/> {client.email}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant={"p"} style={{fontWeight: 250}}>
+                                <CakeIcon color={"primary"}
+                                          style={{verticalAlign: "bottom"}}/> {renderBirthdate()}
+                            </Typography>
+                        </Grid>
                     </Grid>
+                </Grid>
+                <Grid item xs={8}>
 
                 </Grid>
-
             </Grid>
-        );
-    }
+        </Grid>
+    );
 }
 
 const mapStateToProps = state => {
